07686937e74fcf37a646bbb5b5797dd2c7fe1606
Urban Roland <urolir@gmail.com>
2024-04-14 11:18:02 +0200

    project init

---
+++ src/main/java/com/cadmus/LibraryManagementSoapServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cadmus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSoapServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementSoapServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cadmus/LibraryManagementSoapServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadmus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSoapServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
